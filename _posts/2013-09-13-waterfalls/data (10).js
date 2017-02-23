d6168ac8f29f2ad74cbfbc3f1198407c0159d4a2
anota <dada-yd@126.com>
2017-02-23 14:20:02 +0800

    增加瀑布流demo

---
+++ _posts/2013-09-13-waterfalls/data (10).js
@@ -0,0 +1,10 @@
+// JavaScript Document
+
+{
+	code : 0,
+	list : [
+		{src:['images/mod (1).jpg','images/mod (2).jpg','images/mod (3).jpg','images/mod (20).jpg'],title:['111111111','222222222','333333333333','333333333333']},
+		{src:['images/mod (4).jpg','images/mod (5).jpg','images/mod (6).jpg','images/mod (21).jpg'],title:['444444444','555555555555','6666666666','333333333333']},
+		{src:['images/mod (7).jpg','images/mod (8).jpg','images/mod (9).jpg','images/mod (22).jpg'],title:['77777777777','8888888888888','999999999999999','333333333333']},
+	]
+}
