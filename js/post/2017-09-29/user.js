6a02c0bdaf54be82fc544c9d4d64a6783e9c9f3c
anota <dada-yd@126.com>
2017-10-01 12:25:48 +0800

    es6特性demo

---
+++ js/post/2017-09-29/user.js
@@ -0,0 +1,17 @@
+var name1 = 'leo';
+var name2 = 'zMouse';
+var name3 = 'momo';
+var name4 = 'duoduo';
+
+// export name1;
+export { name1, name2, name3, name4 };
+
+// export defaule
+// function() {
+//     return { name1, name2 };
+// }
+
+// export * from "./user2.js";
+// export { name1, name2 };
+
+// export default name1;
---
+++ js/post/2017-09-29/user2.js
@@ -0,0 +1,3 @@
+var zmouse = '钟毅';
+var wuyaowang = '童斌';
+export { zmouse, wuyaowang };
